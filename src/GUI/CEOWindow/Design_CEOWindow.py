a4323be8e9db47a5f6a340856e1bcf6532ea42de
JarkZhang <includezzi@126.com>
2019-06-16 23:47:34 +0800

    更新CEOwindow

--- src/GUI/CEOWindow/Design_CEOWindow.py
+++ src/GUI/CEOWindow/Design_CEOWindow.py
@@ -15,10 +15,35 @@ from PyQt5.Qt import QSplashScreen, QDateTime
 from GUI.CEOWindow.UI.Ui_CEOWindow import Ui_CEOWindow
 
 class Design_CEOWindow(QWidget,Ui_CEOWindow):
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.m_flag = True
+            self.m_Position = event.globalPos() - self.pos()  # 获取鼠标相对窗口的位置
+            event.accept()
+            self.setCursor(QCursor(Qt.OpenHandCursor))  # 更改鼠标图标
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if Qt.LeftButton and self.m_flag:
+            self.move(QMouseEvent.globalPos() - self.m_Position)  # 更改窗口位置
+            QMouseEvent.accept()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.m_flag = False
+        self.setCursor(QCursor(Qt.ArrowCursor))
+
     def __init__(self, parent=None):
         super(Design_CEOWindow, self).__init__(parent)
         self.setupUi(self)
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+        self.btnMenu_Close.clicked.connect(self.close)
+        self.btnAllShop.clicked.connect(lambda :self.stackedWidget.setCurrentIndex(0))
+        self.btnMyShop.clicked.connect(lambda : self.stackedWidget.setCurrentIndex(1))
+        self.btnInf.clicked.connect(lambda : self.stackedWidget.setCurrentIndex(2))
+        self.stackedWidget.setCurrentIndex(0)
+
+
+
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
 
